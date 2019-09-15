98bb066f23814b71a8c7fe9b13c3be0366d62d7b
alvinlo92 <alvinlo92@yahoo.com>
2019-09-14 17:20:33 -0700

    Create schema for postgres and cassandra
    Generate CSV data to load into schemas

---
+++ database/postgresql/schema.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS reviews;
+
+CREATE DATABASE reviews;
+
+\connect reviews;
+
+CREATE TABLE property (
+  id SERIAL PRIMARY KEY,
+  host_picture VARCHAR(100) NOT NULL,
+  host_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE guest (
+  id SERIAL PRIMARY KEY,
+  guest_picture VARCHAR(100) NOT NULL,
+  guest_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE review (
+  id SERIAL PRIMARY KEY,
+  property_id INT NOT NULL,
+  guest_id INT NOT NULL,
+  review TEXT NOT NULL,
+  review_response TEXT NOT NULL,
+  date DATE,
+  accuracy SMALLINT NOT NULL,
+  communication SMALLINT NOT NULL,
+  cleanliness SMALLINT NOT NULL,
+  location SMALLINT NOT NULL,
+  check_in SMALLINT NOT NULL,
+  value INT NOT NULL,
+  FOREIGN KEY (property_id) REFERENCES property (id),
+  FOREIGN KEY (guest_id) REFERENCES guest (id)
+);
