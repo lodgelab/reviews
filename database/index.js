3744b3e03bcd460c8678bf5722beb07c4c746aed
alvinlo92 <alvinlo92@yahoo.com>
2019-09-20 20:46:58 -0700

    Complete CRUD operations for cassandra
    Change cassandra schema to have every data stored in one table
    Change server to use new crud operations

--- database/index.js
+++ database/index.js
@@ -1,63 +1,63 @@
-const mysql = require('mysql');
+const cassandra = require('cassandra-driver');
 
-const connection = mysql.createConnection({ //docker exec -it mysql bash
-  // host: '172.17.0.2', // change this for aws ec2 instance, docker network inspect bridge
-  user: 'root',
-  password: '',
-  database: 'reviewsmodule',
-});
+const client = new cassandra.Client({ contactPoints: ['127.0.0.1:9042'], localDataCenter: 'datacenter1', keyspace: 'reviews' });
 
-connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
-});
-
-const getListingReviews = function (listingID, callback) {
-  connection.query(`SELECT * FROM reviews WHERE listings_id = ${listingID}`, (error, results, fields) => {
-    if (error) {
-      callback(error);
-    } else {
-      callback(null, results);
-    }
-  });
+const getReviews = (propertyId, cb) => {
+  const query = 'SELECT * FROM review WHERE property_id = ?';
+  client.execute(query, [propertyId], { prepare: true })
+    .then((result) => cb(null, result.rows));
 };
 
-const getListingHost = function (listingID, callback) {
-  connection.query(`SELECT host_pic, host_name FROM listings WHERE id = ${listingID}`, (error, results, fields) => {
-    if (error) {
-      callback(error);
-    } else {
-      callback(null, results);
-    }
-  });
+const postReview = (guestReview, propertyId, cb) => {
+  const {
+    id,
+    hostPicture,
+    hostName,
+    guestId,
+    guestPicture,
+    guestName,
+    review,
+    date,
+    accuracy,
+    communication,
+    cleanliness,
+    location,
+    checkIn,
+    value,
+  } = guestReview;
+  const query = 'INSERT INTO review (id, property_id, host_picture, host_name, guest_id, guest_picture, guest_name, review, review_response, date, accuracy, communication, cleanliness, location, check_in, value) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+
+  client.execute(query, [id, propertyId, hostPicture, hostName, guestId, guestPicture, guestName, review, null, date, accuracy, communication, cleanliness, location, checkIn, value], { prepare: true })
+    .then((result) => cb(null, result.rows));
 };
 
-const getReviewUser = function (userID, callback) {
-  connection.query(`SELECT * FROM users WHERE id = ${userID}`, (error, results, fields) => {
-    if (error) {
-      callback(error);
-    } else {
-      callback(null, results);
-    }
+const putReview = (updateReview, propertyId, reviewId, cb) => {
+  let query = ['UPDATE review', 'SET'];
+  const set = [];
+  const values = [];
+  Object.keys(updateReview).forEach((column) => {
+    set.push(`${column} = ?`);
   });
+  Object.values(updateReview).forEach((value) => {
+    values.push(value);
+  });
+  query.push(set.join(', '));
+  query.push(`WHERE id = ${reviewId} AND property_id = ${propertyId}`);
+  query = query.join(' ');
+
+  client.execute(query, values, { prepare: true })
+    .then(() => cb(null));
 };
 
-const getReviewResponse = function (responseID, callback) {
-  connection.query(`SELECT * FROM responses WHERE id = ${responseID}`, (error, results, fields) => {
-    if (error) {
-      callback(error);
-    } else {
-      callback(null, results);
-    }
-  });
+const deleteReview = (propertyId, reviewId, cb) => {
+  const query = 'DELETE FROM review WHERE id = ? AND property_id = ?';
+  client.execute(query, [reviewId, propertyId], { prepare: true })
+    .then(() => cb(null));
 };
 
 module.exports = {
-  connection,
-  getListingReviews,
-  getListingHost,
-  getReviewUser,
-  getReviewResponse,
+  getReviews,
+  postReview,
+  putReview,
+  deleteReview,
 };
