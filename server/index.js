3f3653c6c08710fb3382834779365abf4c48df7b
alvinlo92 <alvinlo92@yahoo.com>
2019-09-26 17:22:23 -0700

    Change express static url

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(express.static('public'));
 app.use('/listings/:property', express.static('public'));
 app.use(morgan('dev'));
 
